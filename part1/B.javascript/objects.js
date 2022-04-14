dcfc09606f4add4c4a5bf403c3a70cbfd748eaaa
samueo <samuel.zairerocha@gmail.com>
2022-04-14 16:21:41 -0300

    meh2

--- part1/B.javascript/objects.js
+++ part1/B.javascript/objects.js
@@ -1,20 +1,20 @@
 const object1 = {
-      name: 'Arto Hellas',
-      age: 35,
-      education: 'PhD',
-    }
-    
-const object2 = {
-name: 'Full Stack web application development',
-level: 'intermediate studies',
-size: 5,
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
 }
-    
+  
+const object2 = {
+    name: 'Full Stack web application development',
+    level: 'intermediate studies',
+    size: 5,
+}
+  
 const object3 = {
-name: {
-      first: 'Dan',
-      last: 'Abramov',
-},
-grades: [2, 3, 5, 3],
-department: 'Stanford University',
+    name: {
+        first: 'Dan',
+        last: 'Abramov',
+    },
+    grades: [2, 3, 5, 3],
+    department: 'Stanford University',
 }
