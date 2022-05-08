e0a9a68a24284c1e9758cada23bcfac355549981
samueo <samuel.zairerocha@gmail.com>
2022-05-07 23:39:55 -0300

    done 1.6

--- part1/1.6/src/App.js
+++ part1/1.6/src/App.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
 
-const History = ({ title, amount }) => <p>{title}: {amount}</p>;
+const History = ({ title, amount }) => {
+  if (amount === 0) {
+    return (
+      <p>{title}: feedback indisponível </p> 
+    )
+  }
+  return (
+    <p>{title}: {amount}</p>
+  )
+}
 
-const Statistic = ({ title, num }) => <p>{title}: {num}</p>;
+const Title = ({ title }) => <h3>{title}</h3>;
 
-const Title = ({ title }) => <h2>{title}</h2>;
-
-const Button = ({ onClick, title }) => (
-  <button onClick={onClick}>
+const Button = ({ eventHandler, title }) => (
+  <button onClick={eventHandler}>
     {title}
   </button>
 );
@@ -18,71 +25,21 @@ function App() {
     neutral: 0,
     bad: 0,
   });
-  
-  const [stats, setStats] = useState({
-    total: 0,
-  });
 
-  const increaseGoodFeedback = () => {
-    setFeedback({
-      ...feedback,
-      good: feedback.good + 1,
-    })
-    setStats({
-      ...stats,
-      total: stats.total + 1,
-    })
-  }
-  
-  const increaseNeutralFeedback = () => {
-    setFeedback({
-      ...feedback,
-      neutral: feedback.neutral + 1,
-    })
-    setStats({
-      ...stats,
-      total: stats.total + 1,
-    })
-  }
-
-  const increaseBadFeedback = () => {
-    setFeedback({
-      ...feedback,
-      bad: feedback.bad + 1,
-    })
-    setStats({
-      ...stats,
-      total: stats.total + 1,  
-    })
-  }
-
-  const avarage = (a, b) => {
-    if (a === 0) {
-      return 0
-    }
-    return Math.abs((a - b)/stats.total)
-  }
-
-  const positive = (x) => {
-    if (x === 0) {
-      return 0 + " %"
-    }
-    return (x/stats.total)*100 + " %"
-  }
+  const increaseGoodFeedback = () => setFeedback({...feedback, good: feedback.good + 1});
+  const increaseNeutralFeedback = () => setFeedback({...feedback, neutral: feedback.neutral + 1});
+  const increaseBadFeedback = () => setFeedback({...feedback, bad: feedback.bad + 1});
 
   return (
     <>
       <Title title={'give feedback'} />
-      <Button onClick={increaseGoodFeedback} title={'good'} /> 
-      <Button onClick={increaseNeutralFeedback} title={'neutral'} /> 
-      <Button onClick={increaseBadFeedback} title={'bad'} /> 
+      <Button eventHandler={increaseGoodFeedback} title={'good'} /> 
+      <Button eventHandler={increaseNeutralFeedback} title={'neutral'} /> 
+      <Button eventHandler={increaseBadFeedback} title={'bad'} /> 
       <Title title={'statistics'} />
       <History title={'good'} amount={feedback.good} />
       <History title={'neutral'} amount={feedback.neutral} />
       <History title={'bad'} amount={feedback.bad} />
-      <Statistic title={'all'} num={stats.total} />
-      <Statistic title={'avarage'} num={avarage(feedback.good, feedback.bad)} />
-      <Statistic title={'positive'} num={positive(feedback.good)} />
     </>
   );
 }
---
+++ part1/1.7/src/App.js
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+const History = ({ title, amount }) => <p>{title}: {amount}</p>;
+
+const Statistic = ({ title, num }) => <p>{title}: {num}</p>;
+
+const Title = ({ title }) => <h2>{title}</h2>;
+
+const Button = ({ onClick, title }) => (
+  <button onClick={onClick}>
+    {title}
+  </button>
+);
+
+function App() {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  
+  const [stats, setStats] = useState({
+    total: 0,
+  });
+
+  const increaseGoodFeedback = () => {
+    setFeedback({
+      ...feedback,
+      good: feedback.good + 1,
+    })
+    setStats({
+      ...stats,
+      total: stats.total + 1,
+    })
+  }
+  
+  const increaseNeutralFeedback = () => {
+    setFeedback({
+      ...feedback,
+      neutral: feedback.neutral + 1,
+    })
+    setStats({
+      ...stats,
+      total: stats.total + 1,
+    })
+  }
+
+  const increaseBadFeedback = () => {
+    setFeedback({
+      ...feedback,
+      bad: feedback.bad + 1,
+    })
+    setStats({
+      ...stats,
+      total: stats.total + 1,  
+    })
+  }
+
+  const avarage = (a, b) => {
+    if (a === 0) {
+      return 0
+    }
+    return Math.abs((a - b)/stats.total)
+  }
+
+  const positive = (x) => {
+    if (x === 0) {
+      return 0 + " %"
+    }
+    return (x/stats.total)*100 + " %"
+  }
+
+  return (
+    <>
+      <Title title={'give feedback'} />
+      <Button onClick={increaseGoodFeedback} title={'good'} /> 
+      <Button onClick={increaseNeutralFeedback} title={'neutral'} /> 
+      <Button onClick={increaseBadFeedback} title={'bad'} /> 
+      <Title title={'statistics'} />
+      <History title={'good'} amount={feedback.good} />
+      <History title={'neutral'} amount={feedback.neutral} />
+      <History title={'bad'} amount={feedback.bad} />
+      <Statistic title={'all'} num={stats.total} />
+      <Statistic title={'avarage'} num={avarage(feedback.good, feedback.bad)} />
+      <Statistic title={'positive'} num={positive(feedback.good)} />
+    </>
+  );
+}
+
+export default App;
